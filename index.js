4ce59e29eeade56219c460b9e366f03c84462d8f
Albert Palau <anorkbcn@gmail.com>
2017-06-12 09:55:52 +0200

    Create index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function summer (sumVal) {
 
 module.exports = summer
 
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-app.get('/',function(req,res) {
-  res.send("Hello World");
-});
+// app.get('/',function(req,res) {
+//   res.send("Hello World");
+// });
 
 // app.listen(4000);
